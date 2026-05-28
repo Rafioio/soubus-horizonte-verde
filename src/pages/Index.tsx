e2bbc59cf81f86058b2dadadbd6d86c13d9a397f
Rafioio <rafaelcampellosoares@gmail.com>
2026-05-28 16:29:55 +0000

    fix: refatoração

---
+++ src/pages/Index.tsx
@@ -0,0 +1,24 @@
+import {About} from "@/components/soubus/About";
+import {Footer} from "@/components/soubus/Footer";
+import {Hero} from "@/components/soubus/Hero";
+import {Navbar} from "@/components/soubus/Navbar";
+import {Problem} from "@/components/soubus/Problem";
+import {Solution} from "@/components/soubus/Solution";
+import {Survey} from "@/components/soubus/Survey";
+import {Team} from "@/components/soubus/Team"
+const Index = () => {
+  return (
+    <div className="min-h-screen bg-background selection:bg-accent">
+      <About />
+      <Survey />
+      <Hero />
+      <Navbar />
+      <Problem />
+      <Solution />
+      <Footer />
+      <Team />
+    </div>
+  );
+};
+
+export default Index;
